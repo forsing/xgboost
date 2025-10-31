085321b53bac7765c43184dac474fa3c0e3d77fe
ForsIng <forsing@gmail.com>
2025-11-01 00:05:41 +0100

    Update loto7_xgboost.py

--- loto7_xgboost.py
+++ loto7_xgboost.py
@@ -248,3 +248,22 @@ print()
 
 # Press Enter to close the window ...
 
+
+
+
+
+
+"""
+=== Qiskit Version Table ===
+Software                       Version        
+---------------------------------------------
+qiskit                         1.4.4          
+qiskit_machine_learning        0.8.3          
+
+=== System Information ===
+Python version                 3.11.13        
+OS Apple                       Darwin 
+"""
+
+
+
